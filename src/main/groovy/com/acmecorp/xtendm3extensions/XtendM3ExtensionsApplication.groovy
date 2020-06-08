4f32a87a3c4be65f02beebe138cbd3a1f3807f91
Erfan Yousefi <er.yousefi@gmail.com>
2020-06-08 15:15:43 +0200

    Fix missing main class for CI

---
+++ src/main/groovy/com/acmecorp/xtendm3extensions/XtendM3ExtensionsApplication.groovy
@@ -0,0 +1,12 @@
+package com.acmecorp.xtendm3extensions
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class XtendM3ExtensionsApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(XtendM3ExtensionsApplication, args)
+  }
+}
